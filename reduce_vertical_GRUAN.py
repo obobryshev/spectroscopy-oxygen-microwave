ccf0a4136f8bb1b2301df46a85ff0b460bbda2e5
Alex Bobryshev <alex.bobryshev@gmail.com>
2021-11-16 13:01:32 +0100

    positive return statement

--- reduce_vertical_GRUAN.py
+++ reduce_vertical_GRUAN.py
@@ -19,7 +19,7 @@ def main():
         '/home/sasha/progs/oxygen_study/spectroscopy-oxygen-microwave/input/lin_1155_2017-10-21T14-34-33.xml',
         '/home/sasha/progs/oxygen_study/spectroscopy-oxygen-microwave/input/lin_1433_2017-10-20T12-10-99.xml'
     ]
-    filenames = ['/home/sasha/progs/oxygen_study/spectroscopy-oxygen-microwave/input/lin_567_2017-10-19T13-29-77.xml']
+    filenames = ['/scratch/uni/u237/users/obobryshev/oxygen_study/02_closure_clean_amsua/noaa19/08_mpm2020/matlab_auxillaries/bar_full_noaa19/bar_806_2017-10-21T19-13-58.xml']
     for ff in filenames:
         test = py.xml.load(ff)
 
@@ -32,7 +32,7 @@ def main():
         oname = os.path.basename(ff[:-4]) + '_vertical50.xml'
         py.xml.save(output, oname)
 
-    print('stop')
+    print('Finished successfully! Bye.')
     return test
 
 
